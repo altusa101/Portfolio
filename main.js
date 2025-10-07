7333e48d5a50b5fd1076e0d024e50989785a94d3
altus101 <altusa101@gmail.com>
2025-10-07 22:11:05 +0530

    a new file made and updated the basic code

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+// the hero sec elements 
+
+function dropDownHeroTxt(){
+    let s1 = document.querySelector(".s1")
+    let s2 = document.querySelector(".s2")
+    let s3 = document.querySelector(".s3")
+    let alls = document.querySelectorAll(".head")
+    s1.style.transform = "translateY(20px)"
+    s2.style.transform = "translateY(-40px)"
+    s3.style.transform = "translateY(-80px)"
+    alls.forEach((es)=>{
+        es.style.opacity = "100%";
+    })
+}
+
+setTimeout(dropDownHeroTxt,2000)
